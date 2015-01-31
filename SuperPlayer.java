d970f0c0d4c59107f3a5be3aa555d9744ee6f472
gracz21 <kamil.walek21@gmail.com>
2015-01-31 22:28:46 +0100

    Final

--- SuperPlayer.java
+++ SuperPlayer.java
@@ -11,38 +11,11 @@ import put.ai.games.game.Move;
 import put.ai.games.game.Player;
 
 public class SuperPlayer extends Player {
-
-    private Random random=new Random(0xdeadbeef);
     
     @Override
     public String getName() {
         return "Kamil Walkowiak 109714 Jarasz Łojka 114816";
     }
-    
-    // private Move alphaBeta(int imm, Color col, Board board) {
-    // 	Move best = null;
-    // 	if(imm != 0) {
-    // 		List<Move> moves = board.getMovesFor(col);
-    // 		for(int i = 0; i < moves.size(); i++) {
-    // 			Board act_board = board;
-    // 			act_board.doMove(moves.get(i));
-    // 			if(col == Color.PLAYER1)
-    // 				col = Color.PLAYER2;
-    // 			else
-    // 				col = Color.PLAYER1;
-    // 			alphaBeta(imm - 1, col, act_board);
-    // 		}
-    		
-    // 	} else {
-    // 		List<Move> moves = board.getMovesFor(col);
-    // 		for(int i = 0; i < moves.size(); i++) {
-    // 			Board act_board = board;
-    // 			act_board.doMove(moves.get(i));
-    // 			//OCENA HEURYSTYCZNA!!!
-    // 		}
-    // 	}
-    // 	return best;
-    // }
 
     private Move randomMove(Board board) {
         List<Move> moves = board.getMovesFor(getColor());
@@ -55,7 +28,7 @@ public class SuperPlayer extends Player {
             Color other = tmp.getWinner(getColor());
             if (other == getColor())
                 return moves.get(i);
-            if (other == NULL) {
+            if (other == null) {
                 List<Move> otherMoves = tmp.getMovesFor(getOpponent(getColor()));
                 for (int j = 0; j < otherMoves.size(); j++) {
                     Board tmp2 = tmp.clone();
@@ -69,27 +42,27 @@ public class SuperPlayer extends Player {
                 }
             }
         }
-        Random r = new Random(System.currentTimeMillis())
+		Random r = new Random();
         if (loss == moves.size())
             return moves.get(r.nextInt(moves.size()));
         else {
             int zeros = moves.size() - loss;
             int index = r.nextInt(zeros);
             int cnt = 0;
-            for (i = 0; i < moves.size(); i++)
+            for (int i = 0; i < moves.size(); i++)
                 if (notes[i] == 0) {
                     if (cnt == index)
                         return moves.get(i);
                     cnt++;
                 }
         }
-        return NULL;
+        return null;
     }
 
     @Override
     public Move nextMove(Board b) {
-    	getTime();
-        List<Move> moves = b.getMovesFor(getColor());
-        return moves.get(random.nextInt(moves.size()));
+        return randomMove(b);
     }
+    
+    public static void main(String[] args) {}
 }
